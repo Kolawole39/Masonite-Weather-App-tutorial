f116207c7f9ae848b67ba798595f7f64118ba74b
Onael21 <onaelmangabo@gmail.com>
2019-12-10 00:38:34 +0100

    Create a Model for City

---
+++ Weather_app/databases/migrations/2019_12_09_232649_create_cities_table.py
@@ -0,0 +1,19 @@
+from orator.migrations import Migration
+
+
+class CreateCitiesTable(Migration):
+
+    def up(self):
+        """
+        Run the migrations.
+        """
+        with self.schema.create('cities') as table:
+            table.increments('id')
+            table.string('name')
+            table.timestamps()
+
+    def down(self):
+        """
+        Revert the migrations.
+        """
+        self.schema.drop('cities')
